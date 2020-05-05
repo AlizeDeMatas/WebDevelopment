03a5af28f9e9e0fd30931f6d4c5d0ba816451481
AlizeDeMatas <32222634+AlizeDeMatas@users.noreply.github.com>
2020-05-04 23:22:33 -0400

    Add files via upload

---
+++ CPS530LabThree/help.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+</head>
+
+<body>
+<h1>Hello, Welcome to Alize De Matas's (student#500745506) Multiplication Page </h1>
+</body>
+</html>
---
+++ CPS530LabThree/myprog.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+use CGI':standard';
+use strict;
+print "Content-type: text/html\n\n";
+
+print "This is my first Perl program";
